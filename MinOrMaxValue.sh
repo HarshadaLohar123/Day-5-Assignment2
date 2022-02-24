ef0aca82c4a713c11e6923dbb001e39e8551003a
HarshadaLohar123 <harshalohar123@gmail.com>
2022-02-24 18:23:43 +0530

    ADDED:UsingIfElse

---
+++ MinOrMaxValue.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -x
+
+
+a=$((RANDOM%1000))
+b=$((RANDOM%1000))
+c=$((RANDOM%1000))
+d=$((RANDOM%1000))
+e=$((RANDOM%1000))
+
+echo $a $b $c $d $e
+
+# Finding Maximum Number
+
+if [ $a -gt $b ] && [ $a -gt $c ] && [ $a -gt $d ] && [ $a -gt $e ]
+then
+	echo 'A is Big'
+elif [ $b -gt $a ] && [ $b -gt $c ] && [ $b -gt $d ] && [ $b -gt $e ]
+then
+	echo 'B is Big'
+elif [ $c -gt $a ] && [ $c -gt $b ] && [ $c -gt $d ] && [ $c -gt $e ]
+then
+	echo 'C is Big'
+elif [ $d -gt $a ] && [ $d -gt $b ] && [ $d -gt $c ] && [ $d -gt $e ]
+then
+	echo 'D is Big'
+else
+	echo 'E is Big'
+fi
+
+# Finding Minumum Number
+
+if [ $a -lt $b ] && [ $a -lt $c ] && [ $a -lt $d ] && [ $a -lt $e ]
+then
+        echo 'A is Small'
+elif [ $b -lt $a ] && [ $b -lt $c ] && [ $b -lt $d ] && [ $b -lt $e ]
+then
+        echo 'B is Small'
+elif [ $c -lt $a ] && [ $c -lt $b ] && [ $c -lt $d ] && [ $c -lt $e ]
+then
+        echo 'C is Small'
+elif [ $d -lt $a ] && [ $d -lt $b ] && [ $d -lt $c ] && [ $d -lt $e ]
+then
+        echo 'D is Small'
+else
+        echo 'E is Small'
+fi
